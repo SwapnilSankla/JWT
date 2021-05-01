1c7c4bab2a9d1d4c6e50b1e554dd16fdc8113875
Swapnil Sankla <sanklas@thoughtworks.com>
2021-05-01 11:32:45 +0530

    Enable OpenAPI documentation

--- src/main/kotlin/com/swapnilsankla/jwt/ui/controller/UIControllerAdvice.kt
+++ src/main/kotlin/com/swapnilsankla/jwt/ui/controller/UIControllerAdvice.kt
@@ -6,31 +6,37 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.client.HttpClientErrorException
 
-@ControllerAdvice
+@ControllerAdvice(basePackageClasses = [UIController::class])
 class UIControllerAdvice {
 	@ExceptionHandler(CustomerNotFoundException::class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
 	fun handleCustomerNotFound(exception: CustomerNotFoundException): ResponseEntity<Customer> {
 		return ResponseEntity.notFound().build()
 	}
 
 	@ExceptionHandler(UnknownProcessingError::class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	fun handleCustomerNotFound(exception: UnknownError): ResponseEntity<Customer> {
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
 	}
 
 	@ExceptionHandler(InvalidTokenException::class, FailedToGenerateTokenException::class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	fun handleCustomerNotFound(exception: InvalidTokenException): ResponseEntity<Customer> {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
 	}
 
 	@ExceptionHandler(ExpiredJwtException::class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	fun handleJwtExpiredException(exception: ExpiredJwtException): ResponseEntity<Customer> {
 		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
 	}
 
 	@ExceptionHandler(HttpClientErrorException::class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
 	fun handleJwtExpiredException(exception: HttpClientErrorException): ResponseEntity<Customer> {
 		return if(exception.statusCode == HttpStatus.UNAUTHORIZED)
 			ResponseEntity.status(HttpStatus.UNAUTHORIZED).build()
